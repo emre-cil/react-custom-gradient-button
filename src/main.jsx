64ee586e3f515f86afb797d5ec86b9362992ee84
emre cil <info.emrecil@gmail.com>
2022-08-23 17:52:47 +0300

    create: empty file template created.

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GradientButton from './GradientButton';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GradientButton />
+  </React.StrictMode>
+);
