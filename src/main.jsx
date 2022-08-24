f749cb339e9cb81f49c98e0857e8ad6c5a2f57c2
emre cil <info.emrecil@gmail.com>
2022-08-24 11:33:22 +0300

    readme version update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GradientButton from 'react-custom-gradient-button';
+import GradientButton from './lib/index.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div>
